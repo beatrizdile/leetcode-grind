f03aec61fa58d2b20614fdafb9bb9b48a8753891
beatrizdile <beatriz.dile@gmail.com>
2025-03-19 19:53:21 -0300

    remove-duplicates-from-sorted array

---
+++ arrays-and-hashing/remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int removeDuplicates(vector<int>& nums) {
+            int left = 1;
+    
+            for (int right = 1; right < nums.size(); right++) {
+                if (nums[right - 1] != nums[right]) {
+                    nums[left] = nums[right];
+                    left++;
+                }
+            }
+    
+            return left;
+        }
+    };
+    
